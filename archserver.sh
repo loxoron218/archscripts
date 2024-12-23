7544e1919ebbd9269e4f57fc31a6121e30ea2306
loxoron218 <175253954+loxoron218@users.noreply.github.com>
2024-12-23 11:13:34 +0100

    Update archserver.sh
    Upgraded readability
    Simplified recurring commands
    Fixed quite some bugs

--- archserver.sh
+++ archserver.sh
@@ -1,45 +1,80 @@
+#==============================================================================
+# SECTION 1: System Preparation
+#==============================================================================
+
+## Buffer sudo for the whole script
+sudo -v
+while true; do sudo -v; sleep 60; done &
+
+## Configure drives
+sudo sh -c 'echo "/dev/sda1   /mnt/sda1   ext4   defaults   0   2" >> /etc/fstab'
+# sudo sh -c 'echo "/dev/sdb1   /mnt/sdb1   ext4   defaults   0   2" >> /etc/fstab' # Add more drives if necessary
+# sudo sh -c 'echo "/dev/sdc1   /mnt/sdc1   ext4   defaults   0   2" >> /etc/fstab' # Add more drives if necessary
+# sudo sh -c 'echo "/dev/sdd1   /mnt/sdd1   ext4   defaults   0   2" >> /etc/fstab' # Add more drives if necessary
+sudo systemctl daemon-reload
+
 ## Configure pacman
 sudo sed -i 's/#Color/Color/' /etc/pacman.conf
 sudo sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 ## Add repositories
-sudo sh -c 'echo "" >> /etc/pacman.conf'
-sudo sh -c 'echo "[gnome-unstable]" >> /etc/pacman.conf'
-sudo sh -c 'echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf'
-sudo sh -c 'echo "" >> /etc/pacman.conf'
-sudo sh -c 'echo "[kde-unstable]" >> /etc/pacman.conf'
-sudo sh -c 'echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf'
+sudo sh -c 'cat >> /etc/pacman.conf << EOF
+
+[gnome-unstable]
+Include = /etc/pacman.d/mirrorlist
+
+[kde-unstable]
+Include = /etc/pacman.d/mirrorlist
+EOF'
 
 ## Install yay
-sudo pacman -Syyu
-sudo pacman -S git
+sudo pacman -Syyu --noconfirm
+sudo pacman -S --noconfirm git
 git clone https://aur.archlinux.org/yay.git
-( cd yay && makepkg -si )
+(cd yay && makepkg -si --noconfirm)
 sudo rm -rf ~/yay
 
-## Install applications
-yay -S cronie dhcpcd docker docker-compose firewalld openssh powertop
-yay -S bash-completion fastfetch
+#==============================================================================
+# SECTION 2: Package Installation and Configuration
+#==============================================================================
 
-## Configure system apps
-sudo systemctl enable dhcpcd.service
+## Install necessary applilcations
+yay -S --noconfirm cronie dhcpcd docker docker-compose firewalld openssh # Install networkmanager to manage Wi-Fi connections
+
+## Install recommended applications
+yay -S --noconfirm bash-completion fastfetch nano powertop xorg-xset
+
+## Configure dhcpcd
+sudo systemctl enable dhcpcd.service # Enable networkmanager if installed
+
+## Configure fastfetch
 fastfetch --gen-config
-sh -c 'echo "fastfetch" >> ~/.bashrc'
-alias clearfast='clear && fastfetch'
+echo "fastfetch" >> ~/.bashrc
+echo "alias clearfetch='clear && fastfetch'" >> ~/.bashrc
+source ~/.bashrc
+
+#==============================================================================
+# SECTION 3: Duck DNS Configuration
+#==============================================================================
 
 ## Set Duck DNS domain and token
 read -p "Enter your Duck DNS domain: " duck_domain
 read -p "Enter your Duck DNS token: " duck_token
 
 ## Configure Duck DNS
-mkdir ~/duckdns
-echo "echo url=\"https://www.duckdns.org/update?domains=${duck_domain}&token=${duck_token}&ip=&verbose=true\" | curl -k -o ~/duckdns/duck.log -K -" > ~/duckdns/duck.sh
-chmod 700 ~/duckdns/duck.sh
-echo "*/5 * * * * ~/duckdns/duck.sh >/dev/null 2>&1" | crontab -
+mkdir -p ~/server/duckdns
+echo "echo url=\"https://www.duckdns.org/update?domains=${duck_domain}&token=${duck_token}&verbose=true\" | curl -k -o ~/server/duckdns/duck.log -K -" > ~/server/duckdns/duck.sh
+chmod 700 ~/server/duckdns/duck.sh
+echo "*/5 * * * * ~/server/duckdns/duck.sh >/dev/null 2>&1" | crontab -
 sudo systemctl enable cronie.service
+~/server/duckdns/duck.sh
 
-## Configure SSH
+#==============================================================================
+# SECTION 4: SSH Configuration
+#==============================================================================
+
+## Set port for SSH
 blocked_ports=("80" "81" "90" "403" "443" "1900" "2283" "5800" "6881" "7359" "7575" "7878" "8080" "8096" "8282" "8920" "9117")
 while true; do
     random_port=$(shuf -i 1000-9999 -n 1)
@@ -49,26 +84,37 @@ while true; do
         break
     fi
 done
+
+## Edit SSH config
 sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
 sudo sed -i "s/#Port 22/Port ${random_port}/" /etc/ssh/sshd_config
-echo "Using random SSH port: ${random_port}"
 sudo systemctl enable sshd.service
+
+## Congure firewalld
 sudo systemctl enable firewalld.service
 sudo systemctl start firewalld.service
 sudo firewall-cmd --zone=public --add-port=${random_port}/tcp --permanent
+for port in "${blocked_ports[@]}"; do
+    sudo firewall-cmd --zone=public --add-port=${port}/tcp --permanent
+done
 sudo firewall-cmd --reload
 
-## Configure Docker
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
-mkdir ~/docker-compose
+#==============================================================================
+# SECTION 5: Immich preparation
+#==============================================================================
+
+## Set password for immich database
+mkdir ~/server/immich
 read -s -p "Enter your Immich database password: " DB_PASSWORD
-echo "# You can find documentation for all the supported env variables at https://immich.app/docs/install/environment-variables
+
+## Create environment file
+cat > ~/server/immich/.env << EOF
+# You can find documentation for all the supported env variables at https://immich.app/docs/install/environment-variables
 
 # The location where your uploaded files are stored
-UPLOAD_LOCATION=~/docker-compose/immich-app/library
+UPLOAD_LOCATION=~/server/immich/library
 # The location where your database files are stored
-DB_DATA_LOCATION=~/docker-compose/immich-app/postgres
+DB_DATA_LOCATION=~/server/immich/postgres
 
 # To set a timezone, uncomment the next line and change Etc/UTC to a TZ identifier from this list: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List
 # TZ=Etc/UTC
@@ -83,24 +129,37 @@ DB_PASSWORD=${DB_PASSWORD}
 # The values below this line do not need to be changed
 ###################################################################################
 DB_USERNAME=postgres
-DB_DATABASE_NAME=immich" > ~/docker-compose/.env
-curl -L -o ~/docker-compose/hwaccel.transcoding.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.transcoding.yml
-curl -L -o ~/docker-compose/hwaccel.ml.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.ml.yml
-echo "## This installation script sets up various services for a home server using Docker Compose (Homarr, Immich, Nextcloud, Vaultwarden, Jellyfin, Jackett, Radarr, Sabnzbd and Nginx Proxy Manager)
-## I took a combination of linuxserver.io and official Docker containers. Then edited the 'restart' settings for all to have the same policy and sorted all sections to "harmonize" the file.
-## Ensure that this file is placed in the same folder as the .env and other .yml files for Immich to work correctly.
+DB_DATABASE_NAME=immich
+EOF
 
+## Download hardware acceleration files
+curl -L -o ~/server/immich/hwaccel.transcoding.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.transcoding.yml
+curl -L -o ~/server/immich/hwaccel.ml.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.ml.yml
+
+#==============================================================================
+# SECTION 6: Docker configuration
+#==============================================================================
+
+## Start Docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+## Create docker-compose file
+cat > ~/server/immich/docker-compose.yml << 'EOF' 
 services:
   homarr:
     image: ghcr.io/ajnart/homarr:latest
     container_name: homarr
+    environment:
+      - PUID=1000
+      - PGID=1000
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock # Optional, only if you want docker integration
-      - ~/docker-compose/homarr/configs:/app/data/configs
-      - ~/docker-compose/homarr/icons:/app/public/icons
-      - ~/docker-compose/homarr/data:/data
+      - ~/server/homarr/configs:/app/data/configs
+      - ~/server/homarr/icons:/app/public/icons
+      - ~/server/homarr/data:/data
     ports:
-      - '7575:7575'
+      - 7575:7575
     restart: unless-stopped
 ---
 services:      
@@ -111,7 +170,7 @@ services:
       - ${UPLOAD_LOCATION}:/usr/src/app/upload
       - /etc/localtime:/etc/localtime:ro
     ports:
-      - '2283:2283' 
+      - 2283:2283 
     restart: unless-stopped 
     extends:
       file: hwaccel.transcoding.yml
@@ -175,8 +234,8 @@ services:
       - AUTO_UPDATE=true
       - RUN_OPTS=
     volumes:
-      - ~/docker-compose/jackett/data:/config
-      - ~/docker-compose/blackhole:/downloads
+      - ~/server/jackett/data:/config
+      - ~/server/jackett/blackhole:/downloads
     ports:
       - 9117:9117
     restart: unless-stopped
@@ -191,9 +250,9 @@ services:
       - TZ=Etc/UTC
       - JELLYFIN_PublishedServerUrl=http://192.168.0.5
     volumes:
-      - ~/docker-compose/jellyfin/library:/config
-      - ~/docker-compose/tvseries:/data/tvshows
-      - ~/docker-compose/movies:/data/movies
+      - ~/server/jellyfin/library:/config
+      - ~/server/jellyfin/tvseries:/data/tvshows
+      - ~/server/jellyfin/movies:/data/movies
     ports:
       - 8096:8096
       - 8920:8920
@@ -204,15 +263,19 @@ services:
 services:
   makemkv:
     image: jlesage/makemkv
+    container_name: makemkv
+    environment:
+      - PUID=1000
+      - PGID=1000
     volumes:
-      - "~/docker-compose/docker/appdata/makemkv:/config:rw"
-      - "~/docker-compose:/storage:ro"
-      - "~/docker-compose:/output:rw"
+      - ~/server/makemkv/docker/appdata/makemkv:/config:rw
+      - ~/server/makemkv:/storage:ro
+      - ~/server/makemkv:/output:rw
     ports:
-      - "5800:5800"
-    devices:
-      - "/dev/sr0:/dev/sr0"
-      - "/dev/sg2:/dev/sg2"
+      - 5800:5800
+  # devices: # Uncomment if you have optical drives
+      # - /dev/sr0:/dev/sr0 # Uncomment if you have optical drives
+      # - /dev/sg2:/dev/sg2 # Uncomment if you have optical drives
     restart: unless-stopped
 ---
 services:
@@ -224,22 +287,26 @@ services:
       - PGID=1000
       - TZ=Etc/UTC
     volumes:
-      - ~/docker-compose/nextcloud/config:/config
-      - ~/docker-compose/data:/data
+      - ~/server/nextcloud/config:/config
+      - ~/server/nextcloud/data:/data
     ports:
       - 443:443
     restart: unless-stopped
 ---
 services:
-  app:
-    image: 'docker.io/jc21/nginx-proxy-manager:latest'
+  nginx-proxy-manager:
+    image: docker.io/jc21/nginx-proxy-manager:latest
+    container_name: nginx-proxy-manager
+    environment:
+      - PUID=1000
+      - PGID=1000
     volumes:
-      - data:/data
-      - ~/docker-compose/letsencrypt:/etc/letsencrypt
+      - ~/server/nginx-proxy-manager/data:/data
+      - ~/server/nginx-proxy-manager/letsencrypt:/etc/letsencrypt
     ports:
-      - '90:90' # Changed it to avoid conflict with Vaultwarden
-      - '81:81'
-      - '403:403' # Changed it to avoid conflict with Nextcloud
+      - 90:90 # Changed it to avoid conflict with Vaultwarden
+      - 81:81
+      - 403:403 # Changed it to avoid conflict with Nextcloud
     restart: unless-stopped
 ---
 services:
@@ -253,8 +320,8 @@ services:
       - WEBUI_PORT=8282 # Changed it to avoid conflict with SABnzbd
       - TORRENTING_PORT=6881
     volumes:
-      - ~/docker-compose/qbittorrent/appdata:/config
-      - ~/docker-compose/downloads:/downloads #optional
+      - ~/server/qbittorrent/appdata:/config
+      - ~/server/qbittorrent/downloads:/downloads
     ports:
       - 8282:8282 # Changed it to avoid conflict with SABnzbd
       - 6881:6881
@@ -270,9 +337,9 @@ services:
       - PGID=1000
       - TZ=Etc/UTC
     volumes:
-      - ~/docker-compose/radarr/data:/config
-      - ~/docker-compose/movies:/movies
-      - ~/docker-compose/download-client-downloads:/downloads
+      - ~/server/radarr/data:/config
+      - ~/server/radarr/movies:/movies
+      - ~/server/radarr/download-client-downloads:/downloads
     ports:
       - 7878:7878
     restart: unless-stopped
@@ -286,9 +353,9 @@ services:
       - PGID=1000
       - TZ=Etc/UTC
     volumes:
-      - ~/docker-compose/sabnzbd/config:/config
-      - ~/docker-compose/downloads:/downloads
-      - ~/docker-compose/incomplete/downloads:/incomplete-downloads
+      - ~/server/sabnzbd/config:/config
+      - ~/server/sabnzbd/downloads:/downloads
+      - ~/server/sabnzbd/incomplete/downloads:/incomplete-downloads
     ports:
       - 8080:8080
     restart: unless-stopped
@@ -297,16 +364,34 @@ services:
   vaultwarden:
     image: vaultwarden/server:testing
     container_name: vaultwarden
+    environment:
+      - PUID=1000
+      - PGID=1000
     volumes:
-      - ~/docker-compose/vw-data:/data
+      - ~/server/vaultwarden/vw-data:/data
     ports:
       - 80:80
-    restart: unless-stopped" > ~/docker-compose/docker-compose.yml
-sudo docker compose -f ~/docker-compose/docker-compose.yml up -d
+    restart: unless-stopped
+EOF
 
-## Update
-yay -Rnsu git
-yay -Yc
-sudo pacman -Scc
-yay -Syyu
+## Run docker-compose file
+sudo docker compose -f ~/server/immich/docker-compose.yml up -d
+
+#==============================================================================
+# SECTION 7: Cleanup
+#==============================================================================
+
+## Remove unnecessary files
+yay -Yc --noconfirm
+sudo pacman -Scc --noconfirm
+
+## Update system
+yay -Syyu --noconfirm
+sudo powertop --calibrate
 sudo powertop --auto-tune
+
+## Stop sudo buffer
+pkill -f "sudo -v"
+
+## Remember SSH port
+echo -e "Your SSH port is: $random_port"
