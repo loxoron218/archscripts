01e73424d4430f14debf16466107ab9989b3e10d
enrique <175253954+loxoron218@users.noreply.github.com>
2024-12-23 17:08:17 +0100

    Big update

--- archinstal.sh
+++ archinstal.sh
@@ -1,51 +1,81 @@
+#==============================================================================
+# SECTION 1: System Preparation
+#==============================================================================
+
 ## Configure pacman
 sudo sed -i 's/#Color/Color/' /etc/pacman.conf
 sudo sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
 ## Add repositories
-sudo sh -c 'echo "" >> /etc/pacman.conf'
-sudo sh -c 'echo "[gnome-unstable]" >> /etc/pacman.conf'
-sudo sh -c 'echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf'
-sudo sh -c 'echo "" >> /etc/pacman.conf'
-sudo sh -c 'echo "[kde-unstable]" >> /etc/pacman.conf'
-sudo sh -c 'echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf'
+sudo sh -c 'cat >> /etc/pacman.conf << EOF
+
+[gnome-unstable]
+Include = /etc/pacman.d/mirrorlist
+
+[kde-unstable]
+Include = /etc/pacman.d/mirrorlist
+EOF'
 
 ## Install yay
-sudo pacman -S git
+sudo pacman -Syyu --noconfirm git
 git clone https://aur.archlinux.org/yay.git
-( cd yay && makepkg -si )
+(cd yay && makepkg -si --noconfirm)
 sudo rm -rf ~/yay
 
-## Install and configure DE
-yay -S gdm plymouth
+#==============================================================================
+# SECTION 2: System Configuration
+#==============================================================================
+
+## Configure Graphical User Interface
+yay -S --noconfirm gdm
 gsettings set org.gnome.mutter experimental-features "['autoclose-xwayland' , 'kms-modifiers' , 'scale-monitor-framebuffer' , 'variable-refresh-rate' , 'xwayland-native-scaling']"
 sudo systemctl enable gdm.service
 sudo systemctl set-default graphical.target
+
+## Configure Plymouth
+yay -S --noconfirm plymouth
 sudo sed -i 's/^HOOKS=(/HOOKS=(plymouth /' /etc/mkinitcpio.conf
 sudo plymouth-set-default-theme -R bgrt
 sudo sed -i 's/$/ quiet splash/' /boot/loader/entries/*.conf
 
-## Install and configure network
-yay -S network-manager-applet
+## Configure network
+yay -S --noconfirm network-manager-applet
 sudo systemctl enable NetworkManager.service
 
-## Install and configure Bluetooth
+## Configure Bluetooth
 sudo sed -i 's/#AutoEnable=true/AutoEnable=false/' /etc/bluetooth/main.conf
 sudo systemctl enable bluetooth.service
 
-## Install other applications
-sudo pacman -S audacity bleachbit dconf-editor evince fastfetch gnome-calculator gnome-control-center gnome-disk-utility gnome-software gnome-text-editor gnome-tweaks libreoffice-fresh nautilus pdfarranger picard soundconverter strawberry vlc
-sudo pacman -S adw-gtk-theme bash-completion firefox-ublock-origin ffmpegthumbnailer gnome-shell-extension-appindicator gvfs-mtp kdegraphics-thumbnailers power-profiles-daemon powertop ttf-liberation xdg-user-dirs
-yay -S blackbox-terminal extension-manager flatseal localsend-bin mission-center nuclear-player-bin whatsapp-for-linux
-yay -S brother-hll2350dw dcraw-thumbnailer ffmpeg-audio-thumbnailer firefox-arkenfox-autoconfig firefox-extension-bitwarden gnome-shell-extension-bing-wallpaper gnome-shell-extension-blur-my-shell nautilus-open-any-terminal
+#==============================================================================
+# SECTION 3: Package Installation
+#==============================================================================
+
+## Install GUI applications from official repository
+sudo pacman -S --noconfirm audacity bleachbit dconf-editor evince fastfetch gnome-calculator gnome-control-center gnome-disk-utility gnome-software gnome-text-editor gnome-tweaks libreoffice-fresh nautilus pdfarranger picard soundconverter strawberry vlc
+
+## Install terminal applications from official repository
+sudo pacman -S --noconfirm adw-gtk-theme bash-completion firefox-ublock-origin ffmpegthumbnailer gnome-shell-extension-appindicator gvfs-mtp kdegraphics-thumbnailers power-profiles-daemon powertop ttf-liberation xdg-user-dirs xorg-xset
+
+## Install GUI applications from AUR
+yay -S --noconfirm blackbox-terminal extension-manager flatseal localsend-bin mission-center nuclear-player-bin vscodium-bin whatsapp-for-linux
+
+## Install terminal applications from AUR
+yay -S --noconfirm brother-hll2350dw dcraw-thumbnailer ffmpeg-audio-thumbnailer firefox-arkenfox-autoconfig firefox-extension-bitwarden gnome-shell-extension-bing-wallpaper gnome-shell-extension-blur-my-shell nautilus-open-any-terminal
+
+## Install Flatpak applications
 flatpak update
 flatpak install -y adw-gtk3-dark winezgui
+
+#==============================================================================
+# SECTION 4: Package Configuration
+#==============================================================================
 
 ## Hide unwanted desktop icons
 echo "NoDisplay=true" | sudo tee ~/.local/share/applications/avahi-discover.desktop
 echo "NoDisplay=true" | sudo tee ~/.local/share/applications/bssh.desktop bssh.desktop
 echo "NoDisplay=true" | sudo tee ~/.local/share/applications/bvnc.desktop bvnc.desktop
+echo "NoDisplay=true" | sudo tee ~/.local/share/applications/bvnc.desktop codium.desktop
 echo "NoDisplay=true" | sudo tee ~/.local/share/applications/cups.desktop cups.desktop
 echo "NoDisplay=true" | sudo tee ~/.local/share/applications/libreoffice-base.desktop libreoffice-base.desktop
 echo "NoDisplay=true" | sudo tee ~/.local/share/applications/libreoffice-calc.desktop libreoffice-calc.desktop
@@ -61,21 +91,33 @@ echo "NoDisplay=true" | sudo tee ~/.local/share/applications/qvidcap.desktop qvi
 ## Configure printer
 sudo systemctl enable cups.service
 sudo lpadmin -p HLL2350DW -v lpd://192.168.178.67/BINARY_P1 -E
-sudo lpoptions -d HLL2350DW
+sudo lpoptions -d HLL2350DW # Manual configuaration still needed
 
-## Configure other apps
+## Configure fastfetch
 fastfetch --gen-config
 sudo sh -c 'echo "fastfetch" >> ~/.bashrc'
 alias clearfast='clear && fastfetch'
+
+## Configure other apps
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal blackbox
 gsettings set org.gnome.desktop.privacy remember-recent-files false
 gsettings set org.gnome.nautilus.preferences show-image-thumbnails 'always'
 
-## Update
-yay -Rnsu git powertop
+#==============================================================================
+# SECTION 5: Cleanup
+#==============================================================================
+
+## Use powertop
+sudo powertop --calibrate
+sudo powertop --auto-tune
+yay -Rnsu --noconfirm powertop xorg-xset
+
+## Remove unnecessary files
 sudo rm -rf ~/.rustup
 sudo rm -rf /var/cache/powertop
-yay -Yc
-sudo pacman -Scc
-yay -Syyu
-sudo powertop --auto-tune
+yay -Yc --noconfirm
+sudo pacman -Scc --noconfirm
+yay -Syyu --noconfirm
+
+## Stop sudo buffer
+pkill -f "sudo -v"
