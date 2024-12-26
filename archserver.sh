70ad64f2f1d3445fb04f49fa61d34d6ca7729389
loxoron218 <175253954+loxoron218@users.noreply.github.com>
2024-12-26 15:42:22 +0100

    Update archserver.sh
    Way too much to comment

--- archserver.sh
+++ archserver.sh
@@ -2,53 +2,34 @@
 # SECTION 1: System Preparation
 #==============================================================================
 
-## Configure drives
+## Configure drive
 sudo mkdir /mnt/sda1
+sudo mkfs.btrfs -f /dev/sda1 # Don't forget to backup your data!
 sudo mount /dev/sda1 /mnt/sda1
-sudo sh -c 'echo "/dev/sda1 /mnt/sda1 ext4 defaults 0 2" >> /etc/fstab'
+sudo sh -c 'echo "/dev/sda1 /mnt/sda1 btrfs defaults 0 2" >> /etc/fstab'
 sudo systemctl daemon-reload
 
 ## Configure RAID
-# sudo pacman -Syyu --noconfirm mdadm
-# sudo wipefs --all /dev/sda /dev/sdb # Add more if you have more drives
-# sudo mkdir /dev/md0
-# sudo mdadm --create /dev/md0 --level=1 --raid-devices=2 /dev/sda /dev/sdb # Add more if you have more drives
-# sudo mkfs.ext4 /dev/md0
-# sudo mount /dev/md0 /mnt/raid
-# sudo sh -c 'echo "/dev/md0 /mnt/raid ext4 defaults 0 2" >> /etc/fstab'
-# sudo mdadm --detail --scan >> /etc/mdadm.conf
-# sudo mkinitcpio -P
+# sudo mkdir /mnt/raid
+# sudo mkfs.btrfs -f -d raid1 -m raid1 /dev/sda /dev/sdb # Add more devices if you want
+# sudo mount /dev/sda /mnt/raid
+# sudo sh -c 'echo "/dev/sda /mnt/raid btrfs defaults 0 2" >> /etc/fstab'
+# sudo systemctl daemon-reload
 
 ## Configure pacman
 sudo sed -i 's/#Color/Color/' /etc/pacman.conf
 sudo sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 
-## Add repositories
-sudo sh -c 'cat >> /etc/pacman.conf << EOF
-
-[gnome-unstable]
-Include = /etc/pacman.d/mirrorlist
-
-[kde-unstable]
-Include = /etc/pacman.d/mirrorlist
-EOF'
-
-## Install yay
-sudo pacman -Syyu --noconfirm git # Only run pacman -S if you installed mdadm
-git clone https://aur.archlinux.org/yay.git
-(cd yay && makepkg -si --noconfirm)
-sudo rm -rf ~/yay
-
 #==============================================================================
 # SECTION 2: Package Installation and Configuration
 #==============================================================================
 
 ## Install necessary applilcations
-yay -S --noconfirm cronie dhcpcd docker docker-compose firewalld openssh rsync # Install networkmanager to manage Wi-Fi connections
+sudo pacman -Syyu --noconfirm cronie dhcpcd docker docker-compose firewalld openssh rsync # Install networkmanager to manage Wi-Fi connections
 
 ## Install recommended applications
-yay -S --noconfirm bash-completion fastfetch nano powertop xorg-xset
+sudo pacman -S --noconfirm bash-completion btop fastfetch nano powertop xorg-xset
 
 ## Configure dhcpcd
 sudo systemctl enable dhcpcd.service # Enable networkmanager if installed
@@ -63,24 +44,12 @@ source ~/.bashrc
 # SECTION 3: Duck DNS Configuration
 #==============================================================================
 
-## Set Duck DNS domain and token
-read -p "Enter your Duck DNS domain: " duck_domain
-read -p "Enter your Duck DNS token: " duck_token
-
-## Configure Duck DNS
-mkdir -p ~/server/duckdns
-echo "echo url=\"https://www.duckdns.org/update?domains=${duck_domain}&token=${duck_token}&verbose=true\" | curl -k -o ~/server/duckdns/duck.log -K -" > ~/server/duckdns/duck.sh
-chmod 700 ~/server/duckdns/duck.sh
-echo "*/5 * * * * ~/server/duckdns/duck.sh >/dev/null 2>&1" | crontab -
-sudo systemctl enable cronie.service
-~/server/duckdns/duck.sh
-
 #==============================================================================
 # SECTION 4: SSH Configuration
 #==============================================================================
 
 ## Set port for SSH
-blocked_ports=("80" "81" "90" "403" "443" "1900" "2283" "5800" "6881" "7359" "7575" "7878" "8080" "8096" "8282" "8920" "9117")
+blocked_ports=("80" "81" "90" "403" "443" "1900" "2283" "5800" "7359" "7575" "7878" "8080" "8096" "8920" "9443")
 while true; do
     random_port=$(shuf -i 1000-9999 -n 1)
     
@@ -109,17 +78,17 @@ sudo firewall-cmd --reload
 #==============================================================================
 
 ## Set password for immich database
-mkdir ~/server/immich
+mkdir ~/Server/immich
 read -s -p "Enter your Immich database password: " DB_PASSWORD
 
 ## Create environment file
-cat > ~/server/immich/.env << EOF
+cat > ~/Server/immich/.env << EOF ## Change every /home/enrique* to your user's home directory
 # You can find documentation for all the supported env variables at https://immich.app/docs/install/environment-variables
 
 # The location where your uploaded files are stored
-UPLOAD_LOCATION=~/server/immich/library
+UPLOAD_LOCATION=/home/enrique/Server/immich/library
 # The location where your database files are stored
-DB_DATA_LOCATION=~/server/immich/postgres
+DB_DATA_LOCATION=/home/enrique/Server/immich/postgres
 
 # To set a timezone, uncomment the next line and change Etc/UTC to a TZ identifier from this list: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List
 # TZ=Etc/UTC
@@ -138,8 +107,8 @@ DB_DATABASE_NAME=immich
 EOF
 
 ## Download hardware acceleration files
-curl -L -o ~/server/immich/hwaccel.transcoding.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.transcoding.yml
-curl -L -o ~/server/immich/hwaccel.ml.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.ml.yml
+curl -L -o ~/Server/immich/hwaccel.transcoding.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.transcoding.yml
+curl -L -o ~/Server/immich/hwaccel.ml.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.ml.yml
 
 #==============================================================================
 # SECTION 6: Docker configuration
@@ -149,8 +118,29 @@ curl -L -o ~/server/immich/hwaccel.ml.yml https://github.com/immich-app/immich/r
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
+## Set Duck DNS domain and token
+read -p "Enter your Duck DNS domain: " duck_domain
+read -p "Enter your Duck DNS token: " duck_token
+
 ## Create docker-compose file
-cat > ~/server/immich/docker-compose.yml << 'EOF' 
+cat > ~/Server/immich/docker-compose.yml << 'EOF'
+services:
+  duckdns:
+    image: lscr.io/linuxserver/duckdns:latest
+    container_name: duckdns
+    network_mode: host #optional
+    environment:
+      - PUID=1000 #optional
+      - PGID=1000 #optional
+      - TZ=Etc/UTC #optional
+      - SUBDOMAINS=${duck_domain}
+      - TOKEN=${duck_token}
+      - UPDATE_IP=ipv4 # Optional
+      - LOG_FILE=false # Optional
+    volumes:
+      - /path/to/duckdns/config:/config # Optional
+    restart: unless-stopped
+---
 services:
   homarr:
     image: ghcr.io/ajnart/homarr:latest
@@ -160,23 +150,23 @@ services:
       - PGID=1000
     volumes:
       - /var/run/docker.sock:/var/run/docker.sock # Optional, only if you want docker integration
-      - ~/server/homarr/configs:/app/data/configs
-      - ~/server/homarr/icons:/app/public/icons
-      - ~/server/homarr/data:/data
+      - /home/enrique/Server/homarr/configs:/app/data/configs
+      - /home/enrique/Server/homarr/icons:/app/public/icons
+      - /home/enrique/Server/homarr/data:/data
     ports:
       - 7575:7575
     restart: unless-stopped
 ---
 services:      
   immich-server:
-    image: ghcr.io/immich-app/immich-server:${IMMICH_VERSION:-release}  
+    image: ghcr.io/immich-app/immich-server:${IMMICH_VERSION:-release}
     container_name: immich_server
     volumes:
       - ${UPLOAD_LOCATION}:/usr/src/app/upload
       - /etc/localtime:/etc/localtime:ro
     ports:
       - 2283:2283 
-    restart: unless-stopped 
+    restart: unless-stopped
     extends:
       file: hwaccel.transcoding.yml
       service: quicksync
@@ -191,7 +181,7 @@ services:
     image: ghcr.io/immich-app/immich-machine-learning:${IMMICH_VERSION:-release}-openvino
     container_name: immich_machine_learning
     volumes:
-      - model-cache:/cache
+      - /home/enrique/Server/immich/model-cache:/cache
     restart: unless-stopped
     extends:
       file: hwaccel.ml.yml
@@ -225,25 +215,6 @@ services:
       -c max_wal_size=2GB
       -c shared_buffers=512MB
       -c wal_compression=on
-volumes:
-  model-cache:
----
-services:
-  jackett:
-    image: lscr.io/linuxserver/jackett:latest
-    container_name: jackett
-    environment:
-      - PUID=1000
-      - PGID=1000
-      - TZ=Etc/UTC
-      - AUTO_UPDATE=true
-      - RUN_OPTS=
-    volumes:
-      - ~/server/jackett/data:/config
-      - ~/server/jackett/blackhole:/downloads
-    ports:
-      - 9117:9117
-    restart: unless-stopped
 ---
 services:
   jellyfin:
@@ -255,9 +226,9 @@ services:
       - TZ=Etc/UTC
       - JELLYFIN_PublishedServerUrl=http://192.168.0.5
     volumes:
-      - ~/server/jellyfin/library:/config
-      - ~/server/jellyfin/tvseries:/data/tvshows
-      - ~/server/jellyfin/movies:/data/movies
+      - /home/enrique/Server/jellyfin/library:/config
+      - /mnt/sda1/Filme:/data/movies # Change directory if you are using RAID
+      - /mnt/sda1/Musik:/data/music # Change directory if you are using RAID
     ports:
       - 8096:8096
       - 8920:8920
@@ -273,9 +244,9 @@ services:
       - PUID=1000
       - PGID=1000
     volumes:
-      - ~/server/makemkv/docker/appdata/makemkv:/config:rw
-      - ~/server/makemkv:/storage:ro
-      - ~/server/makemkv:/output:rw
+      - /home/enrique/Server/makemkv/docker/appdata/makemkv:/config:rw
+      - /home/enrique/Server/makemkv:/storage:ro
+      - /mnt/sda1:/output:rw # Change directory if you are using RAID
     ports:
       - 5800:5800
     # devices: # Uncomment if you have optical drives
@@ -292,8 +263,8 @@ services:
       - PGID=1000
       - TZ=Etc/UTC
     volumes:
-      - ~/server/nextcloud/config:/config
-      - ~/server/nextcloud/data:/data
+      - /home/enrique/Server/nextcloud/config:/config
+      - /home/enrique/Server/nextcloud/data:/data
     ports:
       - 443:443
     restart: unless-stopped
@@ -306,8 +277,8 @@ services:
       - PUID=1000
       - PGID=1000
     volumes:
-      - ~/server/nginx-proxy-manager/data:/data
-      - ~/server/nginx-proxy-manager/letsencrypt:/etc/letsencrypt
+      - /home/enrique/Server/nginx-proxy-manager/data:/data
+      - /home/enrique/Server/nginx-proxy-manager/letsencrypt:/etc/letsencrypt
     ports:
       - 90:90 # Changed it to avoid conflict with Vaultwarden
       - 81:81
@@ -315,22 +286,17 @@ services:
     restart: unless-stopped
 ---
 services:
-  qbittorrent:
-    image: lscr.io/linuxserver/qbittorrent:latest
-    container_name: qbittorrent
+  portainer:
+    image: portainer/portainer-ce:latest
+    container_name: portainer
     environment:
       - PUID=1000
       - PGID=1000
-      - TZ=Etc/UTC
-      - WEBUI_PORT=8282 # Changed it to avoid conflict with SABnzbd
-      - TORRENTING_PORT=6881
     volumes:
-      - ~/server/qbittorrent/appdata:/config
-      - ~/server/qbittorrent/downloads:/downloads
+        - /home/enrique/Server/portainer/data:/data
+        - /var/run/docker.sock:/var/run/docker.sock
     ports:
-      - 8282:8282 # Changed it to avoid conflict with SABnzbd
-      - 6881:6881
-      - 6881:6881/udp
+      - 9443:9443
     restart: unless-stopped
 ---
 services:
@@ -342,9 +308,9 @@ services:
       - PGID=1000
       - TZ=Etc/UTC
     volumes:
-      - ~/server/radarr/data:/config
-      - ~/server/radarr/movies:/movies
-      - ~/server/radarr/download-client-downloads:/downloads
+      - /home/enrique/Server/radarr/data:/config
+      - /mnt/sda1/Filme:/movies
+      - /mnt/sda1:/downloads
     ports:
       - 7878:7878
     restart: unless-stopped
@@ -358,9 +324,9 @@ services:
       - PGID=1000
       - TZ=Etc/UTC
     volumes:
-      - ~/server/sabnzbd/config:/config
-      - ~/server/sabnzbd/downloads:/downloads
-      - ~/server/sabnzbd/incomplete/downloads:/incomplete-downloads
+      - /home/enrique/Server/sabnzbd/config:/config
+      - /mnt/sda1:/downloads
+      - /mnt/sda1/sabnzbd/incomplete:/incomplete-downloads
     ports:
       - 8080:8080
     restart: unless-stopped
@@ -373,14 +339,25 @@ services:
       - PUID=1000
       - PGID=1000
     volumes:
-      - ~/server/vaultwarden/vw-data:/data
+      - /home/enrique/Server/vaultwarden/vw-data:/data
     ports:
       - 80:80
+    restart: unless-stopped
+---
+services:
+  watchtower:
+    image: containrrr/watchtower
+    container_name: watchtower
+    environment:
+      - PUID=1000
+      - PGID=1000
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
     restart: unless-stopped
 EOF
 
 ## Run docker-compose file
-sudo docker compose -f ~/server/immich/docker-compose.yml up -d
+sudo docker compose -f ~/Server/immich/docker-compose.yml up -d
 
 #==============================================================================
 # SECTION 7: Backup creation
@@ -388,23 +365,24 @@ sudo docker compose -f ~/server/immich/docker-compose.yml up -d
 
 ## Setup backup
 sudo chown -R $(whoami) /mnt/sda1 # Change directory if you are using RAID
-echo "rsync -avh --delete ~/server /mnt/sda1/" > ~/server/backup_server.sh # Change directory if you are using RAID
-chmod +x ~/server/backup_server.sh  
-(crontab -l 2>/dev/null; echo "0 3 * * * ~/server/backup_server.sh") | crontab -
-~/server/backup_server.sh
+echo "rsync -avh --delete ~/Server /mnt/sda1/" > ~/Server/server_backup.sh # Change directory if you are using RAID
+chmod +x ~/Server/server_backup.sh
+(crontab -l 2>/dev/null; echo "0 3 * * * ~/Server/server_backup.sh") | crontab -
+sudo chown -R $(whoami) ~/Server
+~/Server/server_backup.sh
 
 #==============================================================================
 # SECTION 8: Cleanup
 #==============================================================================
 
 ## Remove unnecessary files
-yay -Yc --noconfirm
+sudo pacman -Qdtq | awk '{print $1}' | xargs -r sudo pacman -Rns --noconfirm
 sudo pacman -Scc --noconfirm
 sudo rm -rf ~/.cache/go-build
 sudo rm -rf ~/.config/go
 
 ## Update system
-yay -Syyu --noconfirm
+sudo pacman -Syyu --noconfirm
 sudo powertop --calibrate
 sudo powertop --auto-tune
 
