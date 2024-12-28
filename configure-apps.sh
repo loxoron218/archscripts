98ca28d65fbbbcd639d8ba9cef0b4de47df129fa
loxoron218 <175253954+loxoron218@users.noreply.github.com>
2024-12-28 14:50:48 +0100

    Update configure-apps.sh

--- configure-apps.sh
+++ configure-apps.sh
@@ -12,6 +12,15 @@ echo "*/5 * * * * ~/duckdns/duck.sh >/dev/null 2>&1" | crontab -
 sudo systemctl enable cronie.service
 ~/duckdns/duck.sh
 
+## Setup Server Backup
+sudo mkdir /mnt/sda1/server # Change directory if you are using RAID
+echo "rsync -avh --delete --exclude='~/server/immich/postgres' ~/ /mnt/sda1/server" > ~/server/server_backup.sh
+chmod 700 ~/server/server_backup.sh
+sudo chown -R $(whoami) /mnt/sda1 # Change directory if you are using RAID
+sudo chown -R $(whoami) ~/
+(crontab -l 2>/dev/null; echo "0 3 * * * ~/server/server_backup.sh") | crontab -
+~/server/server_backup.sh
+
 ## Configure Vaultwarden Web
 sudo sed -i 's/# WEB_VAULT_FOLDER=\/usr\/share\/webapps\/vaultwarden-web/WEB_VAULT_FOLDER=\/usr\/share\/webapps\/vaultwarden-web/' /etc/vaultwarden.env
 sudo sed -i 's/WEB_VAULT_ENABLED=false/WEB_VAULT_ENABLED=true/' /etc/vaultwarden.env
